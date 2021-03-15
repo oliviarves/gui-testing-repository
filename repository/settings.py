96672ceb0d85948128aeca9a07af5848e2fbd9d0
oliviarves <olivia.rves@gmail.com>
2021-03-15 23:25:24 +0100

    Add 'django_filters' to INSTALLED_APPS

--- repository/settings.py
+++ repository/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'papers'
+    'papers',
+    'django_filters'
 ]
 
 MIDDLEWARE = [

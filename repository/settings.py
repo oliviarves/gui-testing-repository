aa81187ccbc6ee0b068dbf22c93af3acc2be2547
oliviarves <olivia.rves@gmail.com>
2021-03-17 21:22:05 +0100

    Changing db settings

--- repository/settings.py
+++ repository/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -81,9 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': "repository",
         'OPTIONS': {
-            "user": "olivia",
-            "password": "olivia94",
-            'init_command': 'SET default_storage_engine=INNODB'
+            'read_default_file': 'db.cnf'
         }
     }
 }
@@ -126,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')

ad32d2d7240bfa2325957347a58b9c8be1ca0081
oliviarves <olivia.rves@gmail.com>
2021-04-19 05:53:39 +0200

    Set static file

--- repository/urls.py
+++ repository/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from papers import views as papers_views
-from repository import settings
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
